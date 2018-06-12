133b9d7726eda587c9c396d4b71d277fdda1056b
Quek Yu Yang <quekyuyang@yahoo.com>
2018-06-12 08:14:18 +0200

    Makes spring attach to sides instead of center

--- code_assignment10.m
+++ code_assignment10.m
@@ -1,5 +1,5 @@
 %%
-syms w h m g k c l_nat Ix Iy Iz real
+syms w h r m g k c l_nat Ix Iy Iz real
 syms rho cd A real % constants for drag
 syms x y z alpha beta gamma      real
 syms t                       real
@@ -14,6 +14,18 @@ syms Talpha Tbeta Tgamma real
 l_nat = h/2;
 k = 5.8*m*g/2/(h-h*l_nat/sqrt(h^2+(w/2)^2));
 c = 2*sqrt(k*m)/10;
+
+% theta_0 = atan(w/2/h);
+% eqns_1 = (k*(h/cos(theta_0) - l_nat)*cos(theta_0)*2 - m*g)/m - 4.8*g == 0;
+% eqns_2 = 0 + 1/2*k*(h/cos(theta_0) - l_nat)^2*2 - (h + w/2)*m*g - 1/2*k*(sqrt(2)*w/2 - l_nat)^2*2 == 0;
+% eqns = [eqns_1;eqns_2];
+% var = [k;l_nat];
+% param = [m g w h];
+% param_value = [420 9.81 18 25];
+% eqns = subs(eqns,param,param_value);
+% var = subs(var,param,param_value);
+% [k,l_nat] = solve(eqns,var);
+% %c = 2*sqrt(k*m)/10;
 
 %% b. Build equation of motion
 % Linear forces
@@ -31,21 +43,23 @@ rot_mat = [q0^2+q1^2-q2^2-q3^2, 2*(q1*q2-q0*q3)    , 2*(q1*q3-q0*q2);
 
 %euler = [alpha; beta; gamma];
 %eulerd = [alphad; betad; gammad];
+%rel_com = [-0.01,0.01,-0.1]; % center of mass relative to sphere center
 
-rel_com = [-0.01,0.01,-0.1]; % center of mass relative to sphere center
-
+com_deviate = [0;0;0];
+spring1_attach = [x;y;z] + rot_mat*([0;r;0] - com_deviate);
+spring2_attach = [x;y;z] + rot_mat*([0;-r;0] - com_deviate);
+spring1_vec = [0;w/2;h] - spring1_attach; % spring expressed as a vector
+spring2_vec = [0;-w/2;h] - spring2_attach;
 
 % spring
-C_spring1 = sqrt(x^2 + (y-w/2)^2 + (z-h)^2) - l_nat;
-C_spring2 = sqrt(x^2 + (y+w/2)^2 + (z-h)^2) - l_nat;
+C_spring1 = norm(spring1_vec) - l_nat;
+C_spring2 = norm(spring2_vec) - l_nat;
 C_spring1_jac = simplify(jacobian(C_spring1,u))';
 C_spring2_jac = simplify(jacobian(C_spring2,u))';
 F_spring1 = k*C_spring1;
 F_spring2 = k*C_spring2;
 
 % damping
-spring1_vec = [0; w/2 - x; h - y]; % vector parallel to spring
-spring2_vec = [0; -w/2 - x; h - y];
 spring1_vec = spring1_vec/norm(spring1_vec); % normalize to unit vector
 spring2_vec = spring2_vec/norm(spring2_vec);
 spring1_vel = (ud'*spring1_vec)*spring1_vec; % project velocity onto direction of spring
@@ -67,17 +81,19 @@ m = 420;
 g = 9.81;
 h = 25;
 w = 18;
+r = 1;
 rho = 1.25;
 A = pi;
 cd = 0.5;
+udd = subs(udd);
 
 % Time step parameters
 step_size = 0.01;
-total_t = 10;
+total_t = 5;
 t = 0:step_size:total_t;
 
 % Initialize motion
-X_init = [0;0;0;0;0;0];
+X_init = [0;0;0;0;0;0;1;0;0;0];
 [X,Xd] = initializeMotion(X_init,udd,t);
 X = RK4(X,udd,step_size,t);
 
@@ -93,6 +109,10 @@ function [X,Xd] = initializeMotion(X_init,qdd,t)
     xd = X_init(4);
     yd = X_init(5);
     zd = X_init(6);
+    q0 = X_init(7);
+    q1 = X_init(8);
+    q2 = X_init(9);
+    q3 = X_init(10);
 
     qdd = double(subs(qdd));
     
@@ -107,6 +127,10 @@ function Xd = findGradient(X,qdd)
     xd = X(4);
     yd = X(5);
     zd = X(6);
+    q0 = 0;
+    q1 = 0;
+    q2 = 0;
+    q3 = 0;
 
     qdd = double(subs(qdd));
     Xd = [xd; yd; zd; qdd];
@@ -146,7 +170,7 @@ r = [ ...
 end
 
 function dist = point2line(point,line_vec)
-    line2point_vec = 
+    %line2point_vec = 
 
 
 
--- draw.m
+++ draw.m
@@ -1,6 +1,7 @@
 function draw(X)
     h = 25;
     w = 18;
+    r = 1;
 
     for i = 1:size(X,2)
         x = X(1,i);
@@ -10,7 +11,8 @@ function draw(X)
         column_tip1 = [0,-w/2,h];
         column_tip2 = [0,w/2,h];
 
-        plot([column_tip1(2),y,column_tip2(2)],[column_tip1(3),z,column_tip2(3)]);
+        plot([column_tip1(2),y - r],[column_tip1(3),z]); hold on
+        plot([y + r,column_tip2(2)],[z,column_tip2(3)]); hold off
         axis([-10 10 -5 40]);
         daspect([1 1 1]);
         drawnow
